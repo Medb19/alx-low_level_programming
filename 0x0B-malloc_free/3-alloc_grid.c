6af61c1f9f8cb59ee5a80ab18ff00d469ecb9cd2
Medb19 <mbentamou20@gmail.com>
2023-10-05 18:06:15 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,6 +24,7 @@ int **alloc_grid(int width, int height)
 			tab[i] = malloc(sizeof(**tab) * width);
 			if (tab[i] == 0)
 			{
+				/*Everything is free if malloc fails*/
 				while (i--)
 					free(tab[i]);
 				return (NULL);
